4b060c015d00530617688b3bfff24915bd7acb35
isaack <isaackaris.52@gmail.com>
2025-07-05 23:10:25 +0300

    db.ts error handled

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,10 @@
 // db.ts
 const pool = postgres(process.env.DATABASE_URL, {
-  -  host: 'localhost',
-  -  port: 5432,
-  -  username: 'frauduser1',
-  -  password: 'root',
-  -  database: 'fraudshield',
+   host: 'localhost',
+   port: 5432,
+    username: 'frauduser1',
+    password: 'root',
+    database: 'fraudshield',
     ssl: false,
     connection: {
       application_name: 'fraud-shield-app'
